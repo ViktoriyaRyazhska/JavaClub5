b22bb7d80ad740d06b3540ef2e59600a0cc09cc3
Rostyslav <rostylka96@gmail.com>
2022-01-19 16:54:46 +0200

    Add Task2 Classes (not completed)

--- src/main/java/javaclub5/classes/task2/Runner.java
+++ src/main/java/javaclub5/classes/task2/Runner.java
@@ -2,7 +2,7 @@ package javaclub5.classes.task2;
 
 public class Runner {
     public static void main(String[] args) {
-
+        Dinglemouse dinglemouse = Dinglemouse.INST;
+        System.out.println(dinglemouse.plus100(23));
     }
-
 }
