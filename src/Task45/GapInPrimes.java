70acd1e37051b0965e2634b4ffb0f3f2f8c3b5dd
Oleksii Shostak <shostak.ok@gmail.com>
2022-01-19 15:56:43 +0200

    Task 45. Test 45.

---
+++ src/Task45/GapInPrimes.java
@@ -0,0 +1,37 @@
+package Task45;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.LongStream;
+
+class GapInPrimes {
+    public static boolean isPrime(long num) {
+        return num > 1 && LongStream.rangeClosed(2, (long)Math.sqrt(num)).noneMatch(div-> num % div== 0);
+    }
+
+    public static long[] gap(int g, long m, long n) {
+        long[] arr = new long[(int) (n - m + 1)];
+        List<Long> primeNumbers = new ArrayList<Long>();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = m + i;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (isPrime((int) arr[i]))
+                primeNumbers.add((arr[i]));
+            else
+                continue;
+        }
+        arr = new long[2];
+        for (int i = 1; i < primeNumbers.size(); i++) {
+            if ((primeNumbers.get(i) - primeNumbers.get(i - 1)) == g) {
+                arr[0] = primeNumbers.get(i - 1);
+                arr[1] = primeNumbers.get(i);
+                return arr;
+            } else
+                continue;
+        }
+        return null;
+    }
+
+}
---
+++ src/Task45/GapInPrimesTest.java
@@ -0,0 +1,20 @@
+package Task45;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GapInPrimesTest {
+
+    @Test
+    public void test() {
+        System.out.println("Fixed Tests");
+        assertEquals("[101, 103]", Arrays.toString(GapInPrimes.gap(2, 100, 110)));
+        assertEquals("[103, 107]", Arrays.toString(GapInPrimes.gap(4, 100, 110)));
+        assertEquals(null, GapInPrimes.gap(6, 100, 110));
+        assertEquals("[359, 367]", Arrays.toString(GapInPrimes.gap(8, 300, 400)));
+        assertEquals("[337, 347]", Arrays.toString(GapInPrimes.gap(10, 300, 400)));
+    }
+}
