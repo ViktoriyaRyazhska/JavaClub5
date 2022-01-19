d3c1839e1ea6761a7cc69136769f745075ce2471
Volodja Zem <v.zemela@gmail.com>
2022-01-20 00:53:46 +0200

    new Task04

---
+++ src/test/java/four/Task04Test.java
@@ -0,0 +1,20 @@
+package four;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runners.JUnit4;
+
+// TODO: Replace examples and use TDD development by writing your own tests
+
+    public class Task04Test {
+        @Test
+        public void fixedTests() {
+            assertTrue(Task04.isDivisible(4050,27));
+            assertTrue(Task04.isDivisible(10000,20));
+            assertTrue(Task04.isDivisible(10005,1));
+            assertFalse(Task04.isDivisible(4066,27));
+            assertFalse(Task04.isDivisible(10005,2));
+        }
+    }
+
