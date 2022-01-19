1e85c77fafb0b5f5a74b3b61023465578bfc0ab1
Oleksii Shostak <shostak.ok@gmail.com>
2022-01-19 15:41:39 +0200

    Task 40. Test 40.

---
+++ src/Task40/Positive.java
@@ -0,0 +1,14 @@
+package Task40;
+
+public class Positive{
+
+    public static int sum(int[] arr){
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]>0)
+                sum += arr[i];
+        }
+        return sum;
+    }
+
+}
---
+++ src/Task40/PositiveTest.java
@@ -0,0 +1,17 @@
+package Task40;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PositiveTest {
+
+    @Test
+    public void testSomething() {
+        assertEquals(15, Positive.sum(new int[]{1, 2, 3, 4, 5}));
+        assertEquals(13, Positive.sum(new int[]{1, -2, 3, 4, 5}));
+        assertEquals(0, Positive.sum(new int[]{}));
+        assertEquals(0, Positive.sum(new int[]{-1, -2, -3, -4, -5}));
+        assertEquals(9, Positive.sum(new int[]{-1, 2, 3, 4, -5}));
+    }
+}
