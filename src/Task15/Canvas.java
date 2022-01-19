360905a0b4db70bf6532d0a47de9b7b78f917622
Oleksii Shostak <shostak.ok@gmail.com>
2022-01-19 15:12:24 +0200

    Task 15. Test 15.

--- src/Task15/Canvas.java
+++ src/Task15/Canvas.java
@@ -1,47 +1,137 @@
 package Task15;
 
+import java.util.stream.IntStream;
+
 public class Canvas {
-    int width;
-    int height;
+    private int width;
+    private int height;
+    private int fullLength;
+    private StringBuilder theCanvas;
+    private String[][] canvasMatrix;
 
-    public Canvas(int width, int height) {
-        this.width=width;
-        this.height=height;
+    public Canvas(int width, int height){
+        if (width < 1 || height < 1) throw new IllegalArgumentException();
+
+        this.width = width;
+        this.height = height;
+        this.fullLength = width + 4;
+        theCanvas = new StringBuilder();
+        canvasMatrix = new String[fullLength][height + 2];
     }
 
-    public Canvas draw(int x1, int y1, int x2, int y2) {
+    public Canvas draw(int x1, int y1, int x2, int y2) throws IllegalArgumentException {
+
+        if (x1 > height || x1 < 0) throw new IllegalArgumentException();
+        if (x2 > height || x2 < 0) throw new IllegalArgumentException();
+        if (y1 > width || y1 < 0) throw new IllegalArgumentException();
+        if (y2 > width || y2 < 0) throw new IllegalArgumentException();
+
+        if (y1 == y2) drawVertical(x1, y1, x2);
+        else if (x1 == x2) drawHorizontal(x1, y1, y2);
+        else {
+            drawRectangle(x1, y1, x2, y2);
+        }
+
         return this;
     }
 
-    public Canvas fill(int x, int y, char ch) {
+    private void drawRectangle(int x1, int y1, int x2, int y2) {
+        drawVertical(x1, y1, x2 - 1);
+        drawVertical(x1, y2 + 1, x2 - 1);
+        drawHorizontal(x1, y1, y2 + 1);
+        drawHorizontal(x2 - 1, y1, y2 + 1);
+    }
+
+    private void drawHorizontal(int x1, int y1, int y2) throws ArrayIndexOutOfBoundsException {
+        IntStream.rangeClosed(y1 + 1, y2 + 1)
+                .forEachOrdered(col -> canvasMatrix[col][x1 + 1] = "x");
+    }
+
+    private void drawVertical(int x1, int y1, int x2) throws IllegalArgumentException, ArrayIndexOutOfBoundsException {
+        try {
+            IntStream.rangeClosed(x1 + 1, x2 + 1)
+                    .forEachOrdered(row -> canvasMatrix[y1 + 1][row] = "x");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Index out of bounds");
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
+
+    public Canvas fill(int x, int y, char ch) throws IllegalArgumentException {
+
+        if (x < 0 || x > height) throw new IllegalArgumentException();
+        if (y < 0 || y > width) throw new IllegalArgumentException();
+
+        if (canvasMatrix[x][y] == null) {
+            canvasMatrix[x][y] = String.valueOf(ch);
+            fill(x + 1, y, ch);
+            fill(x - 1, y, ch);
+            fill(x, y + 1, ch);
+            fill(x, y - 1, ch);
+        }
+
         return this;
     }
 
-    public String drawCanvas(int width, int height) {
-       return null;
+    public String drawCanvas() {
+        drawBoarders();
+        canvasMatrix[fullLength - 2][height + 1] = "";
+        canvasMatrix[fullLength - 1][height + 1] = "";
+
+        canvasToString();
+
+        return theCanvas.toString();
     }
 
-    public static void main(String[] args) {
+    private void canvasToString() {
+        theCanvas.setLength(0);
+        for (int row = 0; row < height + 2; row++) {
+            for (int col = 0; col < fullLength; col++) {
+                if (canvasMatrix[col][row].equals("\\")) {
+                    theCanvas.append("\n");
+                    break;
+                } else theCanvas.append(canvasMatrix[col][row]);
+            }
+        }
+    }
+
+
+    private void drawBoarders() {
+        drawTopAndBottom();
+        drawSides();
+        drawTopAndBottom();
+    }
+
+    private void drawSides() {
+        for (int h = 1; h < height + 1; h++) {
+            for (int col = 0; col <= fullLength - 2; col++) {
+                if (col == 0 || col == (fullLength - 3))
+                    canvasMatrix[col][h] = "|";
+                else if (canvasMatrix[col][h] == null)
+                    canvasMatrix[col][h] = " ";
+            }
+            addReturn(fullLength, h);
+        }
 
     }
 
+    private void addReturn(int fullLength, int h) {
+        canvasMatrix[fullLength - 2][h] = "\\";
+        canvasMatrix[fullLength - 1][h] = "n";
+    }
+
+    private void drawTopAndBottom() {
+
+        Boolean isTop = true;
+        int row = 0;
+
+        if (canvasMatrix[0][0] == "-") isTop = false;
+        if (!isTop) row = height + 1;
+        for (int i = 0; i < fullLength - 2; i++) {
+            canvasMatrix[i][row] = "-";
+        }
+        if (isTop) {
+            addReturn(fullLength, 0);
+        }
+    }
 }
-
-//    public void drawLines() {
-//        Canvas c = new Canvas(5, 5);
-//        c.draw(0, 2, 4, 2).draw(2, 0, 2, 4);
-//        assertEquals("-------\n|  x  |\n|  x  |\n|xxxxx|\n|  x  |\n|  x  |\n-------", c.drawCanvas());
-//    }
-//
-//    public void drawRectangle() {
-//        Canvas c = new Canvas(7, 7);
-//        c.draw(1, 1, 5, 4);
-//        assertEquals("---------\n|       |\n| xxxxx |\n| x   x |\n| x   x |\n| xxxxx |\n|       |\n|       |\n---------", c.drawCanvas());
-//    }
-//
-//    public void fill() {
-//        Canvas c = new Canvas(7, 7);
-//        c.draw(1, 1, 5, 4).fill(3, 3, 'o');
-//        assertEquals("---------\n|       |\n| xxxxx |\n| xooox |\n| xooox |\n| xxxxx |\n|       |\n|       |\n---------", c.drawCanvas());
-//    }
-//}
