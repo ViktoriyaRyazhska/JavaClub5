b0fda81cc271c12216cf1fee2e6e8055990667e6
Rostyslav <rostylka96@gmail.com>
2022-01-19 12:18:20 +0200

    Task3

--- src/main/java/javaclub5/basicprogram/task3/Runner.java
+++ src/main/java/javaclub5/basicprogram/task3/Runner.java
@@ -1,11 +1,27 @@
 package javaclub5.basicprogram.task3;
+/**
+ * Bob needs a fast way to calculate the volume of a cuboid
+ * with three values: length, width and the height of the cuboid.
+ * Write a function to help Bob with this calculation. *
+ *
+ * @author Rostyslav Ros
+ */
+
 
 public class Runner {
     public static void main(String[] args) {
 
     }
+
+    /**
+     * calculate the volume of a cuboid with three values:
+     * @param length - length of cuboid,
+     * @param width - width of cuboid,
+     * @param height - height of cuboid,
+     * @return volume of cuboid
+     */
     public double calculateVolumeOfCuboid(double length, double width, double height) {
         double volume = length * width * height;
-        return 0;
+        return volume;
     }
 }
