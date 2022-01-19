b20f98be1200b47bffb5f2e7f65b5aac3187b98e
Rostyslav <rostylka96@gmail.com>
2022-01-19 12:01:17 +0200

    Maven project

--- src/main/java/javaclub5/basicprogram/task3/Runner.java
+++ src/main/java/javaclub5/basicprogram/task3/Runner.java
@@ -4,5 +4,8 @@ public class Runner {
     public static void main(String[] args) {
 
     }
-
+    public double calculateVolumeOfCuboid(double length, double width, double height) {
+        double volume = length * width * height;
+        return 0;
+    }
 }
