8621281fb2469eef47fde22d27ddcdfab76e8feb
Oleksii Shostak <shostak.ok@gmail.com>
2022-01-19 22:28:18 +0200

    Task 60. Test 60.

---
+++ src/Task60/FakeBinary.java
@@ -0,0 +1,8 @@
+package Task60;
+
+public class FakeBinary {
+
+    public static String fakeBin(String numberString) {
+        return numberString.replaceAll("[0-4]", "0").replaceAll("[5-9]", "1");
+    }
+}
---
+++ src/Task60/FakeBinaryTest.java
@@ -0,0 +1,16 @@
+package Task60;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FakeBinaryTest {
+
+    @Test
+    public void testSomething() {
+        FakeBinary fakeBinary = new FakeBinary();
+        assertEquals("01011110001100111", fakeBinary.fakeBin("45385593107843568"));
+        assertEquals("101000111101101", fakeBinary.fakeBin("509321967506747"));
+        assertEquals("011011110000101010000011011", fakeBinary.fakeBin("366058562030849490134388085"));
+    }
+}
