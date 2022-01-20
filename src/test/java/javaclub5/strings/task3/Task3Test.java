2319d2cc738e5f6ff0ad97b6c0cae8ff1f1287b0
Rostyslav <rostylka96@gmail.com>
2022-01-20 13:23:40 +0200

    Add Tests of Task 3 Strings (completed)

---
+++ src/test/java/javaclub5/strings/task3/Task3Test.java
@@ -0,0 +1,24 @@
+package javaclub5.strings.task3;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * Tests of Task 3 Strings
+ *
+ * @author Rostyslav Ros
+ */
+
+public class Task3Test {
+
+    @Test
+    /**
+     * Test of sayHello method
+     */
+    public void sayHelloTest() {
+        String[] name = {"John", "Smith"};
+        Assert.assertEquals("Hello, John Smith! Welcome to Phoenix, Arizona!",
+                Runner.sayHello(name, "Phoenix", "Arizona"));
+    }
+}
