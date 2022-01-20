f17193e991080d58f2a80e4f1f778a9ac81de7df
Rostyslav <rostylka96@gmail.com>
2022-01-20 11:04:12 +0200

    Add Task2 Classes (completed)

--- src/main/java/javaclub5/classes/task2/Dinglemouse.java
+++ src/main/java/javaclub5/classes/task2/Dinglemouse.java
@@ -1,11 +1,16 @@
 package javaclub5.classes.task2;
 
+/**
+ *
+ * Class created by Codewars
+ */
+
 public class Dinglemouse {
     public static final Dinglemouse INST = new Dinglemouse();
 
     private static int ONE_HUNDRED = 100;
 
-    private final int value;
+    private static int value = ONE_HUNDRED;
 
     private Dinglemouse() {
         value = ONE_HUNDRED;
--- src/main/java/javaclub5/classes/task2/Runner.java
+++ src/main/java/javaclub5/classes/task2/Runner.java
@@ -1,7 +1,29 @@
 package javaclub5.classes.task2;
 
+/**
+ /**
+ *
+ * The code provided has a method plus100 which is supposed to return the number of the parameter incremented by 100.
+ * But it's not working properly.
+ * Task
+ * Fix the code so we can all go home early.
+ * Restrictions
+ *     Do not modify the constructor
+ *     Do not modify the plus100 method
+ *     Do not modify the ONE_HUNDRED declaration
+ *
+ * @author Rostyslav Ros
+ */
+
 public class Runner {
     public static void main(String[] args) {
+        start();
+    }
+    /**
+     * main method of Class
+     *
+     */
+    public static void start() {
         Dinglemouse dinglemouse = Dinglemouse.INST;
         System.out.println(dinglemouse.plus100(23));
     }
