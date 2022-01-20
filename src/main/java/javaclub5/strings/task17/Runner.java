870c397cfc8a0d4be32ff4c8e7836325f2c6f352
Popova Anna <anitapopova1996@gmail.com>
2022-01-20 15:57:25 +0200

    Task 17 Strings

--- src/main/java/javaclub5/strings/task17/Runner.java
+++ src/main/java/javaclub5/strings/task17/Runner.java
@@ -1,17 +1,57 @@
 package javaclub5.strings.task17;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
+ * Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/
+ * toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details)
+ * such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+ * <p>
+ * StringUtils.toAlternativeString("hello world") == "HELLO WORLD"
+ * StringUtils.toAlternativeString("HELLO WORLD") == "hello world"
+ * StringUtils.toAlternativeString("hello WORLD") == "HELLO world"
+ * StringUtils.toAlternativeString("HeLLo WoRLD") == "hEllO wOrld"
+ * StringUtils.toAlternativeString("12345") == "12345" // Non-alphabetical characters are unaffected
+ * StringUtils.toAlternativeString("1a2b3c4d5e") == "1A2B3C4D5E"
+ * StringUtils.toAlternativeString("StringUtils.toAlternatingCase") == "sTRINGuTILS.TOaLTERNATINGcASE"
+ * <p>
+ * As usual, your function/method should be pure, i.e. it should not mutate the original string.
  *
- *
- *
- *
- *
- *
- *
+ * @author Popova Anna
  */
+
+
 public class Runner {
 
     public static void main(String[] args) {
+        start();
+    }
 
+    public static void start() {
+        System.out.println("Enter some word to change it case: ");
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String input = br.readLine();
+            System.out.println("Your changed case in the word: " + changingLetterCase(input));
+        } catch (IOException ex) {
+            System.out.println("Wrong input.");
+            ex.getStackTrace();
+        }
+    }
+
+    public static String changingLetterCase(String word) {
+        String changedCaseInWord = "";
+
+        char[] charArray = word.toCharArray();
+        for (char w : charArray) {
+            if (Character.isUpperCase(w)) {
+                changedCaseInWord += Character.toLowerCase(w);
+            } else {
+                changedCaseInWord += Character.toUpperCase(w);
+            }
+        }
+        return changedCaseInWord;
     }
 }
