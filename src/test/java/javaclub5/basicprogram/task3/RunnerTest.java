1b9549fa508d39754ae30d64a00a0e240b64c851
Popova Anna <anitapopova1996@gmail.com>
2022-01-20 14:17:23 +0200

    Test for Basic Program task 3

---
+++ src/test/java/javaclub5/basicprogram/task3/RunnerTest.java
@@ -0,0 +1,18 @@
+package javaclub5.basicprogram.task3;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RunnerTest {
+
+    @Test
+
+    public void calculateVolumeOfCuboidTest() {
+        double res = 0.01;
+        Assert.assertEquals(20.0, Runner.calculateVolumeOfCuboid(2, 5, 2), res);
+        Assert.assertEquals(30.0, Runner.calculateVolumeOfCuboid(2,7.5,2),res);
+    }
+
+
+}
