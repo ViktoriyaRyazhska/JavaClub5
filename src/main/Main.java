19d0952360443ce18ba5b0130fd1a8cf6dd7d84a
Oleksii Shostak <shostak.ok@gmail.com>
2022-01-21 09:50:05 +0200

    Update Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,20 +1,8 @@
 package main;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Hi there. We've a great manu for you");
-        System.out.println("Please choose the number from 1 to 72");
-
-        int number = scanner.nextInt();
-        while (number < 1 || number > 72 ){
-            System.out.println("Please try again. You've made a input mistake");
-            System.out.println("I'll remind you. You must choose THE NUMBER");
-            number = scanner.nextInt();
-        }
-        System.out.println("Your number is " + number);
-
+        Programm programm = new Programm();
+        programm.run();
     }
 }
