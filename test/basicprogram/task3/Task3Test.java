d19368db6ee942e90e3a47b1a96df496c3d80084
PotatoAnimal <potatoanimals@gmail.com>
2022-01-22 16:10:12 +0200

    test 3

---
+++ test/basicprogram/task3/Task3Test.java
@@ -0,0 +1,19 @@
+package basicprogram.task3;
+
+import javaclub5.basicprogram.task3.Task3;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task3Test {
+    private static final double delta = 0.0001;
+
+    @Test
+    public void examples() {
+        // assertEquals("expected", "actual");
+        assertEquals(4, Task3.CubeVolume(1, 2, 2), delta);
+        assertEquals(63, Task3.CubeVolume(6.3f, 2, 5), delta);
+    }
+
+}
