f49c11de3310d10272a31ac082d19dac3bc36198
23.shved <testshved@gmail.com>
2022-01-22 23:09:51 +0200

    Commit task1

---
+++ src/main/basicprogram/task1/CodeFast.java
@@ -0,0 +1,21 @@
+package main.basicprogram.task1;
+
+import java.util.Scanner;
+
+import main.InterfaceTestClass;
+
+public class CodeFast implements InterfaceTestClass {
+    Scanner scanner = new Scanner(System.in);
+
+    public static int doubleInteger(int i) {
+        i*=2;
+        return i;
+    }
+
+    @Override
+    public void execute() {
+        System.out.println("Code as fast as you can! You need to double the integer and return it");
+        System.out.println("Enter some number: ");
+        System.out.println(CodeFast.doubleInteger(scanner.nextInt()));
+    }
+}
