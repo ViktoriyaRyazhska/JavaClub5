a71e6f2b696b8d0fbac66231fa30b5adfeaffa8f
remiixsp <Battlenet22>
2022-01-23 13:43:19 +0200

    Adding string repeat class into loops package

---
+++ src/main/java/solutions/loops/SheepCounter.java
@@ -0,0 +1,48 @@
+package solutions.loops;
+
+import solutions.Executable;
+
+import java.util.Scanner;
+
+public class SheepCounter implements Executable {
+    Boolean[] arrayOfSheeps;
+
+
+    static public int Counter(Boolean[] arrayOfSheeps)
+    {
+        int counter=0;
+        for (Boolean arrayOfSheep : arrayOfSheeps) {
+            if (arrayOfSheep != null) {
+                if (arrayOfSheep) {
+                    counter += 1;
+                }
+
+            }
+        }
+        return counter;
+    }
+    public void getData()
+    {
+
+        System.out.println("Enter array size: ");
+        int arrSize= new Scanner(System.in).nextInt();
+        if(arrSize == 0)
+        {
+            throw new NegativeArraySizeException("The number must be greater than 0");
+        }
+        else {
+            this.arrayOfSheeps = new Boolean[arrSize];
+            for (int i = 0; i < arrSize; i++) {
+                System.out.println("Enter boolean number: "+(i+1));
+                this.arrayOfSheeps[i] = new Scanner(System.in).nextBoolean();
+            }
+        }
+    }
+
+
+    @Override
+    public void execute() {
+        getData();
+        System.out.println("There are: "+Counter(this.arrayOfSheeps)+" sheep");
+    }
+}
