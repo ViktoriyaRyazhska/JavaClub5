b33b1fc8513bd7bc54afa8982eb582c5ee32c6bf
Dima Prudius <dimaprudius1995@gmail.com>
2022-01-23 15:59:00 +0200

    added task 2 in conditions

---
+++ src/main/java/javaclub5/conditions/task2/DivisibleNb.java
@@ -0,0 +1,44 @@
+package javaclub5.conditions.task2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Create a function that checks if a number n is divisible by two numbers x AND y.
+ * All inputs are positive, non-zero digits.
+ *
+ * @author Dmytro Prudius
+ */
+
+public class DivisibleNb {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        start(br);
+    }
+
+    public static void start(BufferedReader br) {
+        System.out.println("Enter a number that is divisible by two numbers: ");
+        try {
+            System.out.println("Number to be divided");
+            long number = Integer.parseInt(br.readLine());
+            System.out.println("First number to be divided ");
+            long number2 = Integer.parseInt(br.readLine());
+            System.out.println("Second number to be divided ");
+            long number3 = Integer.parseInt(br.readLine());
+            System.out.println("Result: " + isDivisible(number, number2, number3));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static boolean isDivisible(long n, long x, long y) {
+        if (n % x == 0 && n % y == 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+}
