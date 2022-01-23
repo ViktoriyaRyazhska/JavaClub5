9df1e2d361ef3b5f341e32728a97b90bccbf015f
PotatoAnimal <potatoanimals@gmail.com>
2022-01-23 17:28:44 +0200

    Test29 done Task12 done

--- src/javaclub5/MainConsoleProgram.java
+++ src/javaclub5/MainConsoleProgram.java
@@ -6,6 +6,7 @@ import javaclub5.basicprogram.task11.Task11;
 import javaclub5.basicprogram.task2.Task2;
 import javaclub5.basicprogram.task3.Task3;
 import javaclub5.basicprogram.task4.Task4;
+import javaclub5.classes.task12.Task12;
 import javaclub5.conditions.task27.Task27;
 import javaclub5.conditions.task28.Task28;
 import javaclub5.conditions.task29.Task29;
@@ -45,6 +46,9 @@ public class MainConsoleProgram {
                 break;
             case 11:
                 Task11.start(scanner);
+                break;
+            case 12:
+                Task12.start();
                 break;
             case 27:
                 Task27.main(scanner);
