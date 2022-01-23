3157244f394fe20b7fcc938bfbdd56f647f52c60
Dima Prudius <dimaprudius1995@gmail.com>
2022-01-23 15:09:42 +0200

    I added changes to the task 1 in conditions

--- src/main/java/javaclub5/conditions/task1/Runner.java
+++ src/main/java/javaclub5/conditions/task1/Runner.java
@@ -1,5 +1,8 @@
 package javaclub5.conditions.task1;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * Very simple, given an integer or a floating-point number, find its opposite.
  * @author Dmytro Prudius
@@ -7,7 +10,18 @@ package javaclub5.conditions.task1;
 public class Runner{
 
     public static void main(String[] args) {
-        opposite(1);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        start(br);
+    }
+
+    public static void start(BufferedReader br) {
+        System.out.println("Enter pleas number: ");
+        try {
+            int number = Integer.parseInt(br.readLine());
+            System.out.println("Result: " + opposite(number));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public static int opposite(int number) {
