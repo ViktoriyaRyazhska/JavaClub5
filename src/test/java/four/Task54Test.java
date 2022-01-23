1a152598e323e925fcf665ef210ff7ab8ed60d78
Volodja Zem <v.zemela@gmail.com>
2022-01-23 23:32:29 +0200

    new Task54Test

---
+++ src/test/java/four/Task54Test.java
@@ -0,0 +1,14 @@
+package four;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Task54Test {
+    @Test
+    public void tests() {
+        assertEquals(-1, Task54.opposite(1));
+        assertEquals(-14, Task54.opposite(14));
+        assertEquals(14, Task54.opposite(-14));
+    }
+}
