de6367d52430982b16b7de02fad0754db47e9d39
Dima Prudius <dimaprudius1995@gmail.com>
2022-01-23 16:24:01 +0200

    added task 3 in conditions

---
+++ src/main/java/javaclub5/conditions/task3/BooleanToString.java
@@ -0,0 +1,45 @@
+package javaclub5.conditions.task3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+/**
+ * Implement a function which convert the given boolean value into its string representation.
+ *
+ * @author Dmytro Prudius
+ */
+
+public class BooleanToString {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        start(br);
+    }
+
+    public static void start(BufferedReader br) {
+        System.out.println("Enter a logical value: ");
+        boolean value = true;
+        int result = booleanToInt(value);
+        try {
+            result = Integer.parseInt(br.readLine());
+            System.out.println("Result: " + convert(value));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+    private static int booleanToInt(boolean b){
+        if (b)
+            return 1;
+        return 0;
+    }
+
+    public static String convert(boolean b) {
+        if (b) {
+            return "true";
+        } else {
+            return "false";
+        }
+    }
+}
