e69a7194700154514180de64628e3c7b35b3fd21
MxOnEinstein <treynxd@gmail.com>
2022-01-24 15:35:28 +0200

    Task11 Grasshopper - Debug by Maksym Misakovych (finaly)

--- src/main/java/javaclub5/basicprogram/task11/Runner.java
+++ src/main/java/javaclub5/basicprogram/task11/Runner.java
@@ -1,8 +1,42 @@
 package javaclub5.basicprogram.task11;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Grasshopper - Debug
+ *
+ * @author Mysakovych Maksym
+ */
+
 public class Runner {
     public static void main(String[] args) {
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        start(br);
     }
-
+    public static String weatherInfo(int temp) {
+        if (temp > 0)
+            return (temp + " is freezing temperature");
+        else if (temp < 0)
+            return (temp + " is above freezing temperature");
+        else {
+            return null;
+        }
+    }
+    public static int convertToCelsius(int temperature) {
+        double celsius = (temperature - 32) * (5 / 9.0);
+        return (int) celsius;
+    }
+            public static void start(BufferedReader br) {
+                try {
+                    int x;
+                    System.out.println("To convert, wright temperature in (°F): ");
+                    System.out.println(x = convertToCelsius(br.read()));
+                    System.out.println(weatherInfo(x));
+                } catch (IOException e) {
+                    System.out.println("Wrong input");
+                    e.printStackTrace();
+                }
+            }
 }
