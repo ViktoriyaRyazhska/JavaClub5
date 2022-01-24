89ded4a2ef5dea6b80c1102e5062f7a4f27ad20f
MxOnEinstein <treynxd@gmail.com>
2022-01-24 14:37:50 +0200

    Task9 Make Negative by Maksym Misakovych (finaly)

--- src/main/java/javaclub5/basicprogram/task9/Runner.java
+++ src/main/java/javaclub5/basicprogram/task9/Runner.java
@@ -1,6 +1,21 @@
 package javaclub5.basicprogram.task9;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Return to negative your integer
+ *
+ * @author Mysakovych Maksym
+ */
+
 public class Runner {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        start(br);
+    }
+
     public static int makeNegative(final int x) {
         if (x>0){
             return -x;
@@ -9,7 +24,20 @@ public class Runner {
             return x;
         }
     }
+
+    public static void start(BufferedReader br) {
+        try {
+            System.out.println("Enter number to make negative: ");
+            int x = Integer.parseInt(br.readLine());
+            System.out.println(makeNegative(x));
+        } catch (IOException e) {
+            System.out.println("Wrong input");
+            e.printStackTrace();
+        }
+    }
 }
+
+
 
 
 
