58f50da9fc69776d07880a43333406c7b6995ba5
Oleksii Shostak <shostak.ok@gmail.com>
2022-01-24 21:48:29 +0200

    Update View

--- src/main/View.java
+++ src/main/View.java
@@ -5,7 +5,7 @@ public class View {
     public static final String WRONG_INPUT = "Please try again. You've made a input mistake \n" +
                                                           "I'll remind you. You must choose THE NUMBER \n";
     public static final String TRY_AGAIN = "Please choose the number from 1 to 72";
-    public static final String SAY_GOOBYE = "It was a pleasure to serv you :) \n Have a great day!";
+    public static final String SAY_GOOBYE = "It was a pleasure to work with you :) \n Have a great day!";
 
     public void printMessage(String message){
         System.out.println(message);
