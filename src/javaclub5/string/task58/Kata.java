b956fab17acd4d0356112ff0f4995e496d0d1f16
ZSergo <zapadenko.s@gmail.com>
2022-01-24 23:13:21 +0200

    Correct task 58

--- src/javaclub5/string/task58/Kata.java
+++ src/javaclub5/string/task58/Kata.java
@@ -6,10 +6,11 @@ package javaclub5.string.task58;
 public class Kata {
     public static String tripleTrouble(String one, String two, String three) {
         if (one.isEmpty() || two.isEmpty() || three.isEmpty()) throw new NullPointerException("Not null!!!");
-        if(one.length()!=two.length() || two.length()!=three.length()) throw new IllegalArgumentException("All words have to be the same length");
-        String result="";
+        if (one.length() != two.length() || two.length() != three.length())
+            throw new IllegalArgumentException("All words have to be the same length");
+        String result = "";
         for (int i = 0; i < one.length(); i++) {
-            result = result + one.charAt(i)+two.charAt(i)+three.charAt(i);
+            result = result + one.charAt(i) + two.charAt(i) + three.charAt(i);
         }
         return result;
     }
