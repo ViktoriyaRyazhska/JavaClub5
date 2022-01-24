4c532636ec71e4872c9659db317f41fa474fd8d7
Dima Prudius <dimaprudius1995@gmail.com>
2022-01-24 14:19:35 +0200

    added task 12 in strings

---
+++ src/main/java/javaclub5/strings/task12/GrassHopper.java
@@ -0,0 +1,18 @@
+package javaclub5.strings.task12;
+
+
+
+public class GrassHopper {
+    public static String weatherInfo(int temp) {
+        double c = convertToCelsius(temp);
+        if (c > 0)
+            return (c + " is freezing temperature");
+        else
+            return (c + " is above freezing temperature");
+    }
+
+    public static double convertToCelsius(int temperature) {
+        return (temperature - 32) * (5/9.0);
+    }
+
+}
