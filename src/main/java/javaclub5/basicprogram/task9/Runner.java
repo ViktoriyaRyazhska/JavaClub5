5c845008be8986e3636a9b178a731afc7aab045d
MxOnEinstein <treynxd@gmail.com>
2022-01-24 13:33:40 +0200

    Task9 Make Negative by Maksym Misakovych (repush)

--- src/main/java/javaclub5/basicprogram/task9/Runner.java
+++ src/main/java/javaclub5/basicprogram/task9/Runner.java
@@ -1,16 +1,14 @@
 package javaclub5.basicprogram.task9;
 
 public class Runner {
-            public static int rentalCarCost(int d) {
-                int total = 40 * d;
-                if (d >= 7){
-                    return total - 50;
-                } else if (d >= 3){
-                    return total - 20;
-                } else {
-                    return total;
-                }
-            }
+    public static int makeNegative(final int x) {
+        if (x>0){
+            return -x;
+        }
+        else {
+            return x;
+        }
+    }
 }
 
 
