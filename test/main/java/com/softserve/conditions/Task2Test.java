090f8b08a81f2fb0a87f11fbe4966de4bdc515d7
Hawk6567 <igorilnick@gmail.com>
2022-01-25 19:38:40 +0200

    Added task2 condition with tests

---
+++ test/main/java/com/softserve/conditions/Task2Test.java
@@ -0,0 +1,27 @@
+package main.java.com.softserve.conditions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task2Test {
+
+    @Test
+    void isDivisible() {
+        boolean expected = true;
+        boolean input = Task2.isDivisible(20,4,5);
+        assertEquals(expected,input);
+    }
+    @Test
+    void isNotDivisible() {
+        boolean expected = false;
+        boolean input = Task2.isDivisible(-20,4,5);
+        assertEquals(expected,input);
+    }
+    @Test
+    void isNotDivisible2() {
+        boolean expected = false;
+        boolean input = Task2.isDivisible(0,4,5);
+        assertEquals(expected,input);
+    }
+}
