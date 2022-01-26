eee6157f15e01853f74b813573823db1f5aa4f38
Rostylka <rostylka96@gmail.com>
2022-01-26 22:39:59 +0200

    Add Task 2 OOP(not completed) with Test

--- src/main/java/javaclub5/oop/task2/Dih4.java
+++ src/main/java/javaclub5/oop/task2/Dih4.java
@@ -43,22 +43,196 @@ public enum Dih4 {
             case ROTATE_90_ANTICLOCKWISE: {
                 switch (dih4) {
                     case REFLECT_FORWARD_DIAGONAL: {
-                        return REFLECT_REVERSE_DIAGONAL;
+                        return REFLECT_HORIZONTAL;
                     }
-                    case ROTATE_90_ANTICLOCKWISE:
-                    case REFLECT_VERTICAL:
-                    case REFLECT_HORIZONTAL: {
+                    case ROTATE_90_ANTICLOCKWISE: {
+                        return ROTATE_180;
+                    }
+                    case ROTATE_180: {
                         return ROTATE_90_CLOCKWISE;
-                    }
-                    case ROTATE_180:
-                    case IDENTITY: {
-                        return ROTATE_90_ANTICLOCKWISE;
                     }
                     case ROTATE_90_CLOCKWISE: {
                         return IDENTITY;
                     }
-                    case REFLECT_REVERSE_DIAGONAL: {
+                    case REFLECT_VERTICAL: {
+                        return REFLECT_FORWARD_DIAGONAL;
+                    }
+                    case IDENTITY: {
+                        return ROTATE_90_ANTICLOCKWISE;
+                    }
+                    case REFLECT_HORIZONTAL: {
                         return REFLECT_REVERSE_DIAGONAL;
+                    }
+                    case REFLECT_REVERSE_DIAGONAL: {
+                        return REFLECT_VERTICAL;
+                    }
+                }
+            }
+            case ROTATE_180: {
+                switch (dih4) {
+                    case REFLECT_FORWARD_DIAGONAL: {
+                        return REFLECT_REVERSE_DIAGONAL;
+                    }
+                    case ROTATE_90_ANTICLOCKWISE: {
+                        return ROTATE_90_CLOCKWISE;
+                    }
+                    case ROTATE_180: {
+                        return IDENTITY;
+                    }
+                    case ROTATE_90_CLOCKWISE: {
+                        return ROTATE_90_ANTICLOCKWISE;
+                    }
+                    case REFLECT_VERTICAL: {
+                        return REFLECT_HORIZONTAL;
+                    }
+                    case IDENTITY: {
+                        return ROTATE_180;
+                    }
+                    case REFLECT_HORIZONTAL: {
+                        return REFLECT_VERTICAL;
+                    }
+                    case REFLECT_REVERSE_DIAGONAL: {
+                        return REFLECT_FORWARD_DIAGONAL;
+                    }
+                }
+            }
+            case ROTATE_90_CLOCKWISE: {
+                switch (dih4) {
+                    case REFLECT_FORWARD_DIAGONAL: {
+                        return REFLECT_VERTICAL;
+                    }
+                    case ROTATE_90_ANTICLOCKWISE: {
+                        return IDENTITY;
+                    }
+                    case ROTATE_180: {
+                        return ROTATE_90_ANTICLOCKWISE;
+                    }
+                    case ROTATE_90_CLOCKWISE: {
+                        return ROTATE_180;
+                    }
+                    case REFLECT_VERTICAL: {
+                        return REFLECT_REVERSE_DIAGONAL;
+                    }
+                    case IDENTITY: {
+                        return ROTATE_90_CLOCKWISE;
+                    }
+                    case REFLECT_HORIZONTAL: {
+                        return REFLECT_FORWARD_DIAGONAL;
+                    }
+                    case REFLECT_REVERSE_DIAGONAL: {
+                        return REFLECT_HORIZONTAL;
+                    }
+                }
+            }
+            case REFLECT_VERTICAL: {
+                switch (dih4) {
+                    case REFLECT_FORWARD_DIAGONAL: {
+                        return ROTATE_90_CLOCKWISE;
+                    }
+                    case ROTATE_90_ANTICLOCKWISE: {
+                        return REFLECT_REVERSE_DIAGONAL;
+                    }
+                    case ROTATE_180: {
+                        return REFLECT_HORIZONTAL;
+                    }
+                    case ROTATE_90_CLOCKWISE: {
+                        return REFLECT_FORWARD_DIAGONAL;
+                    }
+                    case REFLECT_VERTICAL: {
+                        return IDENTITY;
+                    }
+                    case IDENTITY: {
+                        return REFLECT_VERTICAL;
+                    }
+                    case REFLECT_HORIZONTAL: {
+                        return ROTATE_180;
+                    }
+                    case REFLECT_REVERSE_DIAGONAL: {
+                        return ROTATE_90_ANTICLOCKWISE;
+                    }
+                }
+            }
+            case REFLECT_FORWARD_DIAGONAL: {
+                switch (dih4) {
+                    case REFLECT_FORWARD_DIAGONAL: {
+                        return IDENTITY;
+                    }
+                    case ROTATE_90_ANTICLOCKWISE: {
+                        return REFLECT_VERTICAL;
+                    }
+                    case ROTATE_180: {
+                        return REFLECT_REVERSE_DIAGONAL;
+                    }
+                    case ROTATE_90_CLOCKWISE: {
+                        return REFLECT_HORIZONTAL;
+                    }
+                    case REFLECT_VERTICAL: {
+                        return ROTATE_90_ANTICLOCKWISE;
+                    }
+                    case IDENTITY: {
+                        return REFLECT_FORWARD_DIAGONAL;
+                    }
+                    case REFLECT_HORIZONTAL: {
+                        return ROTATE_90_CLOCKWISE;
+                    }
+                    case REFLECT_REVERSE_DIAGONAL: {
+                        return ROTATE_180;
+                    }
+                }
+            }
+            case REFLECT_HORIZONTAL: {
+                switch (dih4) {
+                    case REFLECT_FORWARD_DIAGONAL: {
+                        return ROTATE_90_ANTICLOCKWISE;
+                    }
+                    case ROTATE_90_ANTICLOCKWISE: {
+                        return REFLECT_FORWARD_DIAGONAL;
+                    }
+                    case ROTATE_180: {
+                        return REFLECT_VERTICAL;
+                    }
+                    case ROTATE_90_CLOCKWISE: {
+                        return REFLECT_REVERSE_DIAGONAL;
+                    }
+                    case REFLECT_VERTICAL: {
+                        return ROTATE_180;
+                    }
+                    case IDENTITY: {
+                        return REFLECT_HORIZONTAL;
+                    }
+                    case REFLECT_HORIZONTAL: {
+                        return IDENTITY;
+                    }
+                    case REFLECT_REVERSE_DIAGONAL: {
+                        return ROTATE_90_CLOCKWISE;
+                    }
+                }
+            }
+            case REFLECT_REVERSE_DIAGONAL: {
+                switch (dih4) {
+                    case REFLECT_FORWARD_DIAGONAL: {
+                        return ROTATE_180;
+                    }
+                    case ROTATE_90_ANTICLOCKWISE: {
+                        return REFLECT_HORIZONTAL;
+                    }
+                    case ROTATE_180: {
+                        return REFLECT_FORWARD_DIAGONAL;
+                    }
+                    case ROTATE_90_CLOCKWISE: {
+                        return REFLECT_VERTICAL;
+                    }
+                    case REFLECT_VERTICAL: {
+                        return ROTATE_90_CLOCKWISE;
+                    }
+                    case IDENTITY: {
+                        return REFLECT_REVERSE_DIAGONAL;
+                    }
+                    case REFLECT_HORIZONTAL: {
+                        return ROTATE_90_ANTICLOCKWISE;
+                    }
+                    case REFLECT_REVERSE_DIAGONAL: {
+                        return IDENTITY;
                     }
                 }
             }
@@ -132,9 +306,6 @@ public enum Dih4 {
         }
         return false;
     }
-
-
-
 
 
     @Override
