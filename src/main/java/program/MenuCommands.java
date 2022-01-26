a3531679290c5bdb30d2207e944dfd442b602d58
late17 <vachilya2003@gmail.com>
2022-01-26 13:28:50 +0200

    betterMenu 1.167 (+-100500 kostiliv)

--- src/main/java/program/MenuCommands.java
+++ src/main/java/program/MenuCommands.java
@@ -37,33 +37,92 @@ public class MenuCommands {
         topics.add("Basic programs");
         namesOfSolutions.add(new ArrayList<>());
         namesOfSolutions.get(0).add("Beginner series number clock");
+        namesOfSolutions.get(0).add("Volume of a cuboid");
+        namesOfSolutions.get(0).add("You can't code under pressure");
+        namesOfSolutions.get(0).add("Grasshopper");
+        namesOfSolutions.get(0).add("MultiplyingTwo");
+        namesOfSolutions.get(0).add("PixelArt");
+        namesOfSolutions.get(0).add("Return to main menu");
         //сюди всі рішення з basicprograms
 
         topics.add("Classes");
         namesOfSolutions.add(new ArrayList<>());
-        //сюди всі рішення з Classes і т.д.
         namesOfSolutions.get(1).add("Block");
+        namesOfSolutions.get(1).add("Battle fighters");
+        namesOfSolutions.get(1).add("Plus 100");
+        namesOfSolutions.get(1).add("Return to main menu");
+
+
+        //сюди всі рішення з Classes і т.д.
 
         topics.add("Collections");
         namesOfSolutions.add(new ArrayList<>());
         namesOfSolutions.get(2).add("Loop bug fixing");
+        namesOfSolutions.get(2).add("Remove even numbers");
+        namesOfSolutions.get(2).add("Return to main menu");
+
+
 
 
         topics.add("Conditions");
         namesOfSolutions.add(new ArrayList<>());
         namesOfSolutions.get(3).add("Switch it up");
+        namesOfSolutions.get(3).add("Making basical math operation");
+        namesOfSolutions.get(3).add("Convert bool to str");
+        namesOfSolutions.get(3).add("Fibonacci numbers modulo 3");
+        namesOfSolutions.get(3).add("IsDivisible");
+        namesOfSolutions.get(3).add("Opposite number");
+        namesOfSolutions.get(3).add("Rock Paper Scissors");
+        namesOfSolutions.get(3).add("Transportation on vacation");
+        namesOfSolutions.get(3).add("Will be enough space");
+        namesOfSolutions.get(3).add("Return to main menu");
+
+
+
 
         topics.add("Loops");
         namesOfSolutions.add(new ArrayList<>());
-        namesOfSolutions.get(4).add("Switch it up");
+        namesOfSolutions.get(4).add("Beginner lost without map");
+        namesOfSolutions.get(4).add("Beginner reduce but grow");
+        namesOfSolutions.get(4).add("CatDogYears");
+        namesOfSolutions.get(4).add("Gap in primes");
+        namesOfSolutions.get(4).add("Grasshopper");
+        namesOfSolutions.get(4).add("My head is in the wrong end");
+        namesOfSolutions.get(4).add("Find nearest square number");
+        namesOfSolutions.get(4).add("Reversed sequence");
+        namesOfSolutions.get(4).add("Count sheep");
+        namesOfSolutions.get(4).add("Repeating the string");
+        namesOfSolutions.get(4).add("Count positive numbers");
+        namesOfSolutions.get(4).add("Return to main menu");
+
+
 
         topics.add("OOP");
         namesOfSolutions.add(new ArrayList<>());
-        namesOfSolutions.get(5).add("Switch it up");
+        namesOfSolutions.get(5).add("Leetspeak");
+        namesOfSolutions.get(5).add("Adam and Eve");
+        namesOfSolutions.get(5).add("Return to main menu");
+
 
         topics.add("Strings");
         namesOfSolutions.add(new ArrayList<>());
-        namesOfSolutions.get(6).add("Switch it up");
+        namesOfSolutions.get(6).add("Alternating case");
+        namesOfSolutions.get(6).add("Correct mistakes");
+        namesOfSolutions.get(6).add("Find the position");
+        namesOfSolutions.get(6).add("Grasshopper");
+        namesOfSolutions.get(6).add("Number to string");
+        namesOfSolutions.get(6).add("Opposite number");
+        namesOfSolutions.get(6).add("Parse int from char");
+        namesOfSolutions.get(6).add("Remove string spaces");
+        namesOfSolutions.get(6).add("Replace all dots");
+        namesOfSolutions.get(6).add("SortAndStar");
+        namesOfSolutions.get(6).add("Triple trouble 2");
+        namesOfSolutions.get(6).add("Welcome to city");
+        namesOfSolutions.get(6).add("Return to main menu");
+
+
+
+
 
 
         topics.add("Exit");
@@ -93,8 +152,8 @@ public class MenuCommands {
         options.put("Count sheep", new SheepCounter());
         options.put("Count positive numbers", new SumOfPositive());
         options.put("Grasshopper summation", new GrassshoperSummation());
-        options.put("Triple trouble 2 ", new TripleTrouble2());
-        options.put("Opposite number ", new OppositeNumbers());
+        options.put("Triple trouble 2", new TripleTrouble2());
+        options.put("Opposite number", new OppositeNumbers());
         options.put("Grasshoper debug", new GrasshopperDebug());
         options.put("Correct mistakes", new CorrectMistakes());
         options.put("Loop bug fixing", new LoopBugFixing());
