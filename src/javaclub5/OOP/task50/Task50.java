08c5fcd8e3337d4f53a114fa4dcc193fb411389a
Nestor <nestor.t721@gmail.com>
2022-01-26 15:43:16 +0200

    OOP. Task 50

--- src/javaclub5/OOP/task50/Task50.java
+++ src/javaclub5/OOP/task50/Task50.java
@@ -14,36 +14,43 @@ public class Task50 {
     public static void main(Scanner scanner) {
         System.out.println("Enter a figure you want to calculate area");
         System.out.println("Rectangle --> 1, Circle --> 2, Triangle --> 3, Square --> 4 : ");
-        int m = scanner.nextInt();
-        switch (m){
-            case 1:
-                System.out.println("Enter high: ");
-                float high = scanner.nextFloat();
-                System.out.println("Enter length: ");
-                float length = scanner.nextFloat();
-                System.out.print("Your area equals: ");
-                System.out.printf("%.2f",getTotalArea(new Rectangle(high,length)));
-                break;
-            case 2:
-                System.out.println("Enter radius: ");
-                float radius = scanner.nextFloat();
-                System.out.print("Your area equals: ");
-                System.out.printf("%.2f", getTotalArea(new Circle(radius)));
-                break;
-            case 3:
-                System.out.println("Enter high: ");
-                float triangleHigh = scanner.nextFloat();
-                System.out.println("Enter side: ");
-                float triangleSide = scanner.nextFloat();
-                System.out.print("Your area equals: ");
-                System.out.printf("%.2f",getTotalArea(new Triangle(triangleHigh,triangleSide)));
-                break;
-            case 4:
-                System.out.println("Enter side: ");
-                float side = scanner.nextFloat();
-                System.out.print("Your area equals: ");
-                System.out.printf("%.2f", getTotalArea(new Square(side)));
-                break;
+
+        try {
+            int m = scanner.nextInt();
+            switch (m) {
+                case 1:
+                    System.out.println("Enter high: ");
+                    float high = scanner.nextFloat();
+                    System.out.println("Enter length: ");
+                    float length = scanner.nextFloat();
+                    System.out.print("Your area equals: ");
+                    System.out.printf("%.2f", getTotalArea(new Rectangle(high, length)));
+                    break;
+                case 2:
+                    System.out.println("Enter radius: ");
+                    float radius = scanner.nextFloat();
+                    System.out.print("Your area equals: ");
+                    System.out.printf("%.2f", getTotalArea(new Circle(radius)));
+                    break;
+                case 3:
+                    System.out.println("Enter high: ");
+                    float triangleHigh = scanner.nextFloat();
+                    System.out.println("Enter side: ");
+                    float triangleSide = scanner.nextFloat();
+                    System.out.print("Your area equals: ");
+                    System.out.printf("%.2f", getTotalArea(new Triangle(triangleHigh, triangleSide)));
+                    break;
+                case 4:
+                    System.out.println("Enter side: ");
+                    float side = scanner.nextFloat();
+                    System.out.print("Your area equals: ");
+                    System.out.printf("%.2f", getTotalArea(new Square(side)));
+                    break;
+            }
+        }catch (Exception e) {
+
+            System.out.println("Put correct number");
+
         }
     }
 }
