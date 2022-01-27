f01874390c2cc339a9bd65e969d195a110c5547d
Dima Prudius <dimaprudius1995@gmail.com>
2022-01-27 14:20:24 +0200

    added test 9 task in strings

---
+++ src/test/java/javaclub5/strings/task9/Task9Test.java
@@ -0,0 +1,11 @@
+package javaclub5.strings.task9;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Task9Test {
+    @Test
+    public void basicTests() {
+        Assert.assertEquals("Position of alphabet: 1",Runner.position('a'));
+    }
+}
