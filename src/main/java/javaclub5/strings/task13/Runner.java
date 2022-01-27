4fa6562bdc0efe1600b627c61d59308a26a5cd4b
MxOnEinstein <treynxd@gmail.com>
2022-01-27 07:36:11 +0200

    Task13/string  - finaly

---
+++ src/main/java/javaclub5/strings/task13/Runner.java
@@ -0,0 +1,30 @@
+package javaclub5.strings.task13;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Abbreviate a Two Word Name
+ *
+ * @author Mysakovych Maksym
+ */
+
+public class Runner {
+    public static String abbrevName(String name) {
+        String[] ab = name.split(" ");
+        return (ab[0].charAt(0) + "." + ab[1].charAt(0)).toUpperCase();
+    }
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        start(br);
+    }
+    public static void start(BufferedReader br) {
+        try {
+            System.out.println("Enter your name and surname: ");
+            String name = br.readLine();
+            System.out.println(abbrevName(name));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
