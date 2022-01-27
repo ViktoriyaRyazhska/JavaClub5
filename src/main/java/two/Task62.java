f4f2a907a00ae3b1be8b0e78fb2ffacbfc62c7e9
Anastasiia <asenka88@gmail.com>
2022-01-27 18:48:10 +0200

    Recoging Task62

--- src/main/java/two/Task62.java
+++ src/main/java/two/Task62.java
@@ -4,14 +4,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 public class Task62 {
+
+        public static double convert(int F) {
+        double celsius = (F - 32) * 5/9.0;
+
+        return celsius;
+    }
+
     public static void task62(BufferedReader reader){
-        System.out.println("Remove string spaces");
-        System.out.println("Enter value");
-        String inputString = null;
+        System.out.println("Debug celsius converter");
+        System.out.println("Enter temperature in fahrenheit");
+
         try {
-            inputString = reader.readLine();
-            String reversedString = inputString.replaceAll("\\s+","");
-            System.out.println(reversedString);
+            int F = Integer.parseInt(reader.readLine());
+            if (convert(F) > 0)
+                System.out.println(convert(F) + " above 0");
+            else
+                System.out.println(convert(F) + " below 0");
 
         } catch (IOException e) {
             e.printStackTrace();
