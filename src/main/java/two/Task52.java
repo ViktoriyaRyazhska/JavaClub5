6de2c5d05e3dbf1d49e05580e1d34ec25c785c2f
Anastasiia <asenka88@gmail.com>
2022-01-27 18:02:41 +0200

    Recoding Task57

--- src/main/java/two/Task52.java
+++ src/main/java/two/Task52.java
@@ -5,19 +5,30 @@ import java.io.IOException;
 import java.util.regex.Pattern;
 
 public class Task52 {
+    public static String reg(String inputString){
+        return ((Pattern.matches("[a-zA-Z]+",inputString)? new StringBuffer(inputString).reverse().toString():"Invalid value" ));
+
+    }
+
     public static void task52(BufferedReader reader){
     System.out.println("Reversed strings");
     System.out.println("Enter value");
-    String inputString = null;
+    //String inputString = null;
     try {
-        inputString = reader.readLine();
+       String inputString = reader.readLine();
+        System.out.println(reg(inputString));
+       /*
         if((Pattern.matches("[a-zA-Z]+",inputString))){
             String reversedString = new StringBuffer(inputString).reverse().toString();
             System.out.println(reversedString);
+
+
         }
         else{
             System.out.println("Invalid value");
-        }
+            }
+
+        */
     } catch (IOException e) {
         e.printStackTrace();
     }
