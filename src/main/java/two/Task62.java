39877cb9ab1945a9e6936206f1672980d656788a
Данило Глинський <Danylo.Hlynskyi.PZ.2020@edu.lpnu.ua>
2022-01-27 19:07:07 +0200

    task 62 tested

--- src/main/java/two/Task62.java
+++ src/main/java/two/Task62.java
@@ -5,23 +5,19 @@ import java.io.IOException;
 
 public class Task62 {
 
-        public static double convert(int F) {
-        double celsius = (F - 32) * 5/9.0;
-
-        return celsius;
+    public static double convert(int F) {
+        return Math.round((F - 32) * 5/9.0);
     }
 
     public static void task62(BufferedReader reader){
         System.out.println("Debug celsius converter");
         System.out.println("Enter temperature in fahrenheit");
-
         try {
             int F = Integer.parseInt(reader.readLine());
             if (convert(F) > 0)
                 System.out.println(convert(F) + " above 0");
             else
                 System.out.println(convert(F) + " below 0");
-
         } catch (IOException e) {
             e.printStackTrace();
         }
