16bf5f628a6fa27f3ac8a7a552ddabedae98b5a2
Данило Глинський <Danylo.Hlynskyi.PZ.2020@edu.lpnu.ua>
2022-01-27 19:15:12 +0200

    task 17 tested

--- src/test/java/two/Task17Test.java
+++ src/test/java/two/Task17Test.java
@@ -4,5 +4,17 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class Task17Test {
-    // this task should be updated
+    @Test
+    public void test01() {
+        Task17 test = new Task17(new int[]{2,2,2});
+        Assert.assertEquals(2, test.getHeight());
+        Assert.assertEquals(2, test.getWidth());
+        Assert.assertEquals(2, test.getLength());
+    }
+    @Test
+    public void test02() {
+        Task17 test = new Task17(new int[]{2,2,2});
+        Assert.assertEquals(24, test.getSurfaceArea());
+        Assert.assertEquals(8, test.getVolume());
+    }
 }
