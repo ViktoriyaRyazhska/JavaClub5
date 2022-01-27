dd466c538cea5ee36ec3e73e2ea123c265978073
Данило Глинський <Danylo.Hlynskyi.PZ.2020@edu.lpnu.ua>
2022-01-27 18:46:38 +0200

    task 52 tested

--- src/main/java/two/Task52.java
+++ src/main/java/two/Task52.java
@@ -6,29 +6,16 @@ import java.util.regex.Pattern;
 
 public class Task52 {
     public static String reg(String inputString){
-        return ((Pattern.matches("[a-zA-Z]+",inputString)? new StringBuffer(inputString).reverse().toString():"Invalid value" ));
-
+        return ((Pattern.matches("[a-zA-Z]+", inputString) ?
+                new StringBuffer(inputString).reverse().toString() : "Invalid value"));
     }
 
     public static void task52(BufferedReader reader){
-    System.out.println("Reversed strings");
+    System.out.println("Reversed string");
     System.out.println("Enter value");
-    //String inputString = null;
     try {
-       String inputString = reader.readLine();
+        String inputString = reader.readLine();
         System.out.println(reg(inputString));
-       /*
-        if((Pattern.matches("[a-zA-Z]+",inputString))){
-            String reversedString = new StringBuffer(inputString).reverse().toString();
-            System.out.println(reversedString);
-
-
-        }
-        else{
-            System.out.println("Invalid value");
-            }
-
-        */
     } catch (IOException e) {
         e.printStackTrace();
     }
