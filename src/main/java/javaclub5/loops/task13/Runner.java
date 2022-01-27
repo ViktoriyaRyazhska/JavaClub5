52069b67e0701ca6ad2b1abffdffd3cfa27af72e
Dmytro4124 <dmytro.grigorovich@gmail.com>
2022-01-27 09:25:34 +0200

    Added task 13 loops

---
+++ src/main/java/javaclub5/loops/task13/Runner.java
@@ -0,0 +1,34 @@
+package javaclub5.loops.task13;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Runner {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        start(br);
+    }
+
+    private static void start(BufferedReader br) {
+        System.out.println("Enter number to sum");
+        try {
+            int n = Integer.parseInt(br.readLine());
+            if(n>0)
+                System.out.println("Result " + summation(n));
+            else
+                System.out.println("Wrong input, must be bigger than 0");
+        } catch (IOException e) {
+            System.out.println("Wrong input");
+            System.out.println(e.getMessage());
+        }
+
+    }
+    public static int summation(int n) {
+        int sum = 0;
+        for(int i = 1; i<=n; i++){
+            sum += i;
+        }
+        return sum;
+    }
+}
