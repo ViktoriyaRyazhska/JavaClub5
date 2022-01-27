241f4ecfa37d79a23428771934268282f9af97d8
Dima Prudius <dimaprudius1995@gmail.com>
2022-01-27 14:32:27 +0200

    added test 16 task in strings

---
+++ src/test/java/javaclub5/strings/task16/Task16Test.java
@@ -0,0 +1,12 @@
+package javaclub5.strings.task16;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Task16Test {
+
+    @Test
+    public void testSomething(){
+        Assert.assertEquals("SOLUTION",Runner.correct(Runner.correct("50LUT10N")));
+    }
+}
