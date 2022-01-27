44b8ef5af2b231c76c66384eee48413fb6dfbcfa
PotatoAnimal <potatoanimals@gmail.com>
2022-01-27 23:28:55 +0200

    test 13 done well

---
+++ test/javaclub5/classes/task13/Task13Test.java
@@ -0,0 +1,12 @@
+package javaclub5.classes.task13;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Task13Test {
+
+    @Test
+    public void test() {
+        System.out.println(Task13.INST.plus100(23));
+}}
