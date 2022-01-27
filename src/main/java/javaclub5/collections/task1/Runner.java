1d0f14a45385fc366d6226f689b4442572410d8a
MxOnEinstein <treynxd@gmail.com>
2022-01-27 13:20:13 +0200

    Task1/Collections  - finaly

---
+++ src/main/java/javaclub5/collections/task1/Runner.java
@@ -0,0 +1,30 @@
+package javaclub5.collections.task1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Push a hash/an object into array
+ *
+ * @author Mysakovych Maksym
+ */
+
+public class Runner {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        start(br);
+    }
+
+    public static void start(BufferedReader reader) {
+        System.out.println("In this task need to add string value.");
+        System.out.println("This is the item pushed into the list: " + push());
+    }
+
+    public static List<String> push() {
+        List<String> items = new ArrayList<>();
+        items.add("apple");
+        return items;
+    }
+}
