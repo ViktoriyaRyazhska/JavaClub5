bb31381227cef30fb864b1b95bba3f1d5795c3cd
Oleksii Shostak <shostak.ok@gmail.com>
2022-01-27 23:03:17 +0200

    Add try/catch blocks

--- src/main/conditions/task25/OppositesAttract.java
+++ src/main/conditions/task25/OppositesAttract.java
@@ -2,6 +2,7 @@ package main.conditions.task25;
 
 import main.InterfaceTestClass;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class OppositesAttract implements InterfaceTestClass {
@@ -15,14 +16,21 @@ public class OppositesAttract implements InterfaceTestClass {
 
     @Override
     public void execute() {
+        int flower1 = 0;
+        int flower2 = 0;
         System.out.println("Timmy & Sarah think they are in love. If one of the flowers has an even number of " +
                 "petals and the other has an odd number of petals it means they are in love.\n");
         System.out.println("We'll find if it's a true love.");
         System.out.println("Specify amount of Timmy's petals");
-        int flower1 = scanner.nextInt();
-        System.out.println("Specify amount of Sarah's petals");
-        int flower2 = scanner.nextInt();
-        System.out.println(OppositesAttract.isLove(flower1, flower2));
+        try {
+            flower1 = scanner.nextInt();
+            System.out.println("Specify amount of Sarah's petals");
+            flower2 = scanner.nextInt();
+            System.out.println(OppositesAttract.isLove(flower1, flower2));
+        }catch (InputMismatchException | NumberFormatException exception){
+            System.out.println("Wrong input");
+        }
+
 
     }
 }
