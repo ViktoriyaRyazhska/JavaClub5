0cf43971b732bb2bf4130fe2b27a29295d879d9b
redminter <oleg.a.fedyshyn@gmail.com>
2022-01-27 17:12:29 +0200

    Test is doning

---
+++ test/collections/task1/tst/Task70test.java
@@ -0,0 +1,13 @@
+package collections.task1.tst;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Task70test {
+    @Test
+    void CreateListTest() {
+        //Assertions.assertAll(() ->assertEquals(
+    }
+}
