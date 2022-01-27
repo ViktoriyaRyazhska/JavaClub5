4b247dca4009c595941a88229a9ba64c5f17ecd5
Данило Глинський <Danylo.Hlynskyi.PZ.2020@edu.lpnu.ua>
2022-01-27 18:49:57 +0200

    optimise imports

--- src/main/java/two/Task57.java
+++ src/main/java/two/Task57.java
@@ -2,23 +2,18 @@ package two;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.regex.Pattern;
 
 public class Task57 {
     public static String reg(String inputString){
         return inputString.replaceAll("\\s+","");
-
     }
 
     public static void task57(BufferedReader reader){
         System.out.println("Remove string spaces");
         System.out.println("Enter value");
-
         try {
            String inputString = reader.readLine();
-          //  String reversedString = inputString.replaceAll("\\s+","");
             System.out.println(reg(inputString));
-
         } catch (IOException e) {
             e.printStackTrace();
         }
