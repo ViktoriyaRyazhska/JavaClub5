c3c56fe5fb7c16daee625890445a314b96e1f810
redminter <oleg.a.fedyshyn@gmail.com>
2022-01-27 16:04:19 +0200

    Test done

--- test/OOP/task49/Task49test.java
+++ test/OOP/task49/Task49test.java
@@ -1,10 +1,28 @@
 package OOP.task49;
 
+import javaclub5.OOP.task49.Leetspeak;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class Task49test {
-    @Test
-    void L() {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+public class Task49test {
+    Leetspeak myEncoder = new Leetspeak();
+    @Test
+    void Leetspeak() {
+
+        Assertions.assertAll(() ->assertEquals("/^^\\"+"0"+"4", myEncoder.encode("moa")),
+                () ->assertEquals("1"+"0"+"(_)", myEncoder.encode("lou")),
+                () ->assertEquals("3"+"1"+"0", myEncoder.encode("elo")),
+                () ->assertEquals("/^^\\"+"1"+"(_)", myEncoder.encode("mlu")));
+
+    }
+
+    @Test
+    void LeetspeakExceptionTest() {
+        assertThrows(NullPointerException.class,
+                () -> myEncoder.encode(null),
+                () -> myEncoder.encode(""));
     }
 }
