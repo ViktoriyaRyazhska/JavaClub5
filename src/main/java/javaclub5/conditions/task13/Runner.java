06b05996e44a198b9f1e0c71713ec5e6778d3be1
MxOnEinstein <treynxd@gmail.com>
2022-01-27 05:53:04 +0200

    Task13/conditions TrafficLights - finaly

---
+++ src/main/java/javaclub5/conditions/task13/Runner.java
@@ -0,0 +1,28 @@
+package javaclub5.conditions.task13;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Runner {
+        public static class TrafficLights{
+                public static String updateLight(String current) {
+                    if(current.equals("green")){return "yellow"; }
+                    if(current.equals("yellow")){return "red"; }
+                    if(current.equals("red")){return "green"; }
+                    else {return "error";}
+                }
+            public static void main(String[] args) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                start(br);
+            }
+            public static void start(BufferedReader br) {
+                try {
+                    System.out.println("Please specify color: ");
+                    String color = br.readLine();
+                    System.out.println(TrafficLights.updateLight(color));
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+}
