f9cfea08b8a68fe6b3e11cb00bff00623acf9be0
PotatoAnimal <potatoanimals@gmail.com>
2022-01-28 03:09:54 +0200

    task71.... test50 ....

---
+++ src/javaclub5/collections/Task71/Task71.java
@@ -0,0 +1,19 @@
+package javaclub5.collections.Task71;
+
+import java.util.List;
+//
+//public class Task71 {
+//    public static void start () {
+//        public static List<Integer> filterOddNumber(List<Integer> listOfNumbers);
+//        {
+//            for (int i = 0; i < listOfNumbers.size(); i++)
+//            {
+//                if (listOfNumbers.get(i)%2 == 0)
+//                {
+//                    listOfNumbers.remove(i);
+//                }
+//            }
+//            return listOfNumbers;
+//        }
+//    }
+//}
