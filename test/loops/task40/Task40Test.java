7608f472205eceb78f50f2792224e56d61ae74e4
destroyrt <eugene.vslv@gmail.com>
2022-01-28 13:11:27 +0200

    Task 40 Test

---
+++ test/loops/task40/Task40Test.java
@@ -0,0 +1,17 @@
+package loops.task40;
+
+import javaclub5.loops.task40.Task40;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class Task40Test {
+    @Test
+    public void testSomething() {
+        assertEquals(15, Task40.reverse(new int[]{1,2,3,4,5}));
+        assertEquals(13, Task40.reverse(new int[]{1,-2,3,4,5}));
+        assertEquals(0, Task40.reverse(new int[]{}));
+        assertEquals(0, Task40.reverse(new int[]{-1,-2,-3,-4,-5}));
+        assertEquals(9, Task40.reverse(new int[]{-1,2,3,4,-5}));
+    }
+}
