e06f5395c30a39d1fe26229384d9bb42d9ad208b
oleksandrhavryush <90772851+oleksandrhavryush@users.noreply.github.com>
2022-01-28 12:09:26 +0200

    Task 60 String fixed

--- src/javaclub5/string/task60/Task60.java
+++ src/javaclub5/string/task60/Task60.java
@@ -19,7 +19,7 @@ public class Task60 {
 
     public static String fakeBin(String numberString) {
         numberString = numberString.replaceAll("[0-4]", "0");
-        numberString = numberString.replaceAll("[5-9]", "5");
+        numberString = numberString.replaceAll("[5-9]", "1");
         return numberString;
     }
 
