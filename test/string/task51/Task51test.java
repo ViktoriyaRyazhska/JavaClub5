d50f06c5548fcb2b7d9052cd1122bc80364af949
destroyrt <eugene.vslv@gmail.com>
2022-01-28 12:20:34 +0200

    Task 51 Test

---
+++ test/string/task51/Task51test.java
@@ -0,0 +1,10 @@
+/*package string.task51;
+
+public class Task51test {
+    @Test
+    public void tests() {
+        assertEquals("67", Kata.numberToString(67));
+        assertEquals("123", Kata.numberToString(123));
+        assertEquals("999", Kata.numberToString(999));
+    }
+}*/
