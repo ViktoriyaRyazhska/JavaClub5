3d24860e327ea3f91980c5d18078cfa574dab226
Dmytro4124 <dmytro.grigorovich@gmail.com>
2022-01-28 09:52:55 +0200

    Added test task 14 conditions

--- src/test/java/javaclub5/conditions/task14/Task14Test.java
+++ src/test/java/javaclub5/conditions/task14/Task14Test.java
@@ -1,4 +1,13 @@
 package javaclub5.conditions.task14;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Task14Test {
+
+    @Test
+    public void rentalCarCostTest(){
+        Assert.assertEquals(40, Runner.rentalCarCost(1));
+        Assert.assertEquals(80, Runner.rentalCarCost(2));
+    }
 }
