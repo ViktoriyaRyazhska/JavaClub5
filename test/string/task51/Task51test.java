be472890d173d5b0d07652e8bc6769e9b2f0629c
destroyrt <eugene.vslv@gmail.com>
2022-01-28 12:24:26 +0200

    Task 51 Test

--- test/string/task51/Task51test.java
+++ test/string/task51/Task51test.java
@@ -1,10 +1,15 @@
-/*package string.task51;
+package string.task51;
+
+import javaclub5.string.task51.Task51;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
 
 public class Task51test {
     @Test
     public void tests() {
-        assertEquals("67", Kata.numberToString(67));
-        assertEquals("123", Kata.numberToString(123));
-        assertEquals("999", Kata.numberToString(999));
+        assertEquals("67", Task51.convertedNum(67));
+        assertEquals("123", Task51.convertedNum(123));
+        assertEquals("999", Task51.convertedNum(999));
     }
-}*/
+}
