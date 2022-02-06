a315d176c7231cee48ae41e9d864020ef84b1fb9
Anastasiia <asenka88@gmail.com>
2022-02-06 22:50:04 +0200

    add staticc WebDriver driver

--- src/test/java/GreenCity/tests/GreenCityRunner.java
+++ src/test/java/GreenCity/tests/GreenCityRunner.java
@@ -1,12 +1,13 @@
 package GreenCity.tests;
 
-import io.netty.channel.DelegatingChannelPromiseNotifier;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.ITest;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -14,24 +15,33 @@ import org.testng.annotations.BeforeMethod;
 import java.time.Duration;
 
 public abstract class GreenCityRunner {
-    protected WebDriver driver;
+    protected static WebDriver driver;
 
     @Before
     public void BeforeSuite() {
-
         System.setProperty("webdriver.chrome.driver", "src\\resources\\chromedriver.exe");
+
     }
     @BeforeClass
-        public void BeforeClass(){
-        //System.out.println("Open page");
-        System.out.println("BeforeClass");
+        public static void setUpBeforeClass() throws Exception{
+        System.out.println("Open page");
+        System.setProperty("webdriver.chrome.driver","src\\resources\\chromedriver.exe" );
         driver = new ChromeDriver();
-        // webDriver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://ita-social-projects.github.io/GreenCityClient/#/");
+
+       // System.out.println("Open dialogue window");
+       // driver.findElement(By.className("header_sign-in-link")).click();
+
+       // System.out.println("Wait for dialogue window");
+       // WebDriverWait driverWait = new WebDriverWait(driver, 30);
+      //g  driverWait.until(ExpectedConditions.presenceOfElementLocated(By.className("cdk-overlay-container")));
         }
 
         @AfterClass
-        public void tearDownAfterClass(){
+        public static void tearDownAfterClass(){
             if (driver != null) {
                 driver.quit();
             }
@@ -48,7 +58,7 @@ public abstract class GreenCityRunner {
             if(!result.isSuccess()){
                 System.out.println("Test "+result.getName()+" Error");
             }
-            driver.get("https://ita-social-projects.github.io/GreenCityClient/#/");
+          //  driver.get("https://ita-social-projects.github.io/GreenCityClient/#/");
         }
 
 
