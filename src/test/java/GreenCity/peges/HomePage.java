b19f0a627e7b86860057bb7ad54246af76952257
Anastasiia <asenka88@gmail.com>
2022-02-06 13:39:30 +0200

    add field switchLanguage to HomePage

--- src/test/java/GreenCity/peges/HomePage.java
+++ src/test/java/GreenCity/peges/HomePage.java
@@ -1,12 +1,20 @@
 package GreenCity.peges;
 
-import GreenCity.PopUpMenu.guest.TopPage;
+import GreenCity.data.Languages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends TopPage {
+
+
     public HomePage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
+    }
+
+    //Functional
+    public HomePage switchLanguage(Languages languages){
+        chooseLanguage(languages);
+        return new HomePage(driver);
     }
 }
