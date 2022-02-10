f2e3e5d2921e42033841b77804efb581c73ff7c8
Данило Глинський <Danylo.Hlynskyi.PZ.2020@edu.lpnu.ua>
2022-02-10 20:08:21 +0200

    main updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,39 @@
 import config.SpringConfig;
-import models.Role;
-import models.User;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import services.implementations.ServiceImp;
-import services.implementations.UserServiceImp;
+
+import models.*;
+import services.implementations.*;
+import services.interfaces.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 
-        UserServiceImp userService = context.getBean("userService", UserServiceImp.class);
-        User user1 = new User("Danylo", "Hlynskyi", "danylohlynsky@gmail.com", "1111", 17, Role.admin);
+        UserService userService = context.getBean("userServiceImp", UserServiceImp.class);
+        AuthorService authorService = context.getBean("authorServiceImp", AuthorServiceImp.class);
+        BookService bookService = context.getBean("bookServiceImp", BookServiceImp.class);
+
+        User user1 = new User("Danylo", "Hlynskyi", "danylohlynsky@gmail.com", "1111", 17, new Role(RoleEnum.ADMIN));
         userService.saveUser(user1);
-        User user2 = new User("name", "surname", "name.surname@gmail.com", "0000", 25, Role.user);
+        User user2 = new User("name", "surname", "name.surname@gmail.com", "0000", 25, new Role(RoleEnum.USER));
         userService.saveUser(user2);
+        userService.saveUser(user2);
+
+        Author author = new Author("Dan", "Brown");
+        authorService.saveAuthor(author);
+        Author author1 = new Author("Taras", "Shevchenko");
+        authorService.saveAuthor(author1);
+
+        Book book = new Book("Origin", 20);
+        List<Author> coAuthors = new ArrayList<>();
+        coAuthors.add(author1);
+        book.setCoAuthors(coAuthors);
+        book.setMainAuthor(author);
+        bookService.saveBook(book);
+
         context.close();
     }
 }
