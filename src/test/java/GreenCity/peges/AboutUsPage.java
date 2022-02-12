037d2801a2ccab2abccb9f7c91a7fe3bec9e3fbc
Anastasiia <asenka88@gmail.com>
2022-02-12 22:26:16 +0200

    Provided scroll void

--- src/test/java/GreenCity/peges/AboutUsPage.java
+++ src/test/java/GreenCity/peges/AboutUsPage.java
@@ -1,5 +1,6 @@
 package GreenCity.peges;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
@@ -7,5 +8,54 @@ public class AboutUsPage extends TopPage {
     public AboutUsPage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
+    }
+    private void scrollDown(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);", getCopyright());
+    }
+
+    //Business logic
+    @Override
+    public HomePage navigateMenuHome() {
+        scrollDown();
+        return super.navigateMenuHome();
+    }
+
+    public EcoNewsPage navigateMenuEcoNews() {
+        scrollDown();
+        return super.navigateMenuEcoNews();
+    }
+    @Override
+    public TipsTricksPage navigateMenuTipsTricks() {
+        scrollDown();
+        return super.navigateMenuTipsTricks();
+    }
+    public EventsPage navigateMenuEvents() {
+        scrollDown();
+        return super.navigateMenuEvents();
+    }
+
+    public PlacesPage navigateMenuPlaces() {
+        scrollDown();
+        return super.navigateMenuPlaces();
+    }
+
+    public AboutUsPage navigateMenuAboutUs() {
+        scrollDown();
+        return super.navigateMenuAboutUs();
+    }
+
+    public MySpacePage navigateMenuMySpace() {
+        scrollDown();
+        return super.navigateMenuMySpace();
+    }
+
+    public UBSCourierPage navigateMenuUBSCourier() {
+        scrollDown();
+        return super.navigateMenuUBSCourier();
+    }
+    public UBSCourierPage navigateGreenCity() {
+        scrollDown();
+        return super.navigateGreenCity();
     }
 }
