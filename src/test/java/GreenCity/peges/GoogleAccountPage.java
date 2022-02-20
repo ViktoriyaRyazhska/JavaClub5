b145e1878c3d0f3fdf14d627a55fbce41a004a71
Anastasiia <asenka88@gmail.com>
2022-02-20 15:56:44 +0200

    Create void clickSingInGoogle()

--- src/test/java/GreenCity/peges/GoogleAccountPage.java
+++ src/test/java/GreenCity/peges/GoogleAccountPage.java
@@ -1,13 +1,25 @@
 package GreenCity.peges;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class GoogleAccountPage {
     private WebDriver driver;
 
+    @FindBy(xpath="//mat-dialog-container[@id='mat-dialog-0']/app-auth-modal/div/div/div[2]/div/app-sign-up/form/app-google-btn/button/span")
+    protected WebElement singInGoogle;
+
     public GoogleAccountPage(WebDriver driver){
         this.driver=driver;
         PageFactory.initElements(driver, this);
+    }
+    public WebElement getSingInGoogle() {
+        return singInGoogle;
+    }
+
+    public void clickSingInGoogle() {
+        getSingInGoogle().click();
     }
 }
