9067e5bf7cbb0da3f1dd286e7493b40cca204326
Anastasiia <asenka88@gmail.com>
2022-02-21 12:22:22 +0200

    data for registration

---
+++ src/test/java/GreenCity/data/RegistrationData.java
@@ -0,0 +1,61 @@
+package GreenCity.data;
+
+import java.util.ArrayList;
+
+public class RegistrationData {
+
+    public static ArrayList<String> getGuestData(){
+    ArrayList<String> guestData = new ArrayList<>();
+    guestData.add("nastacy@yahoo.com");
+    guestData.add("1q2w3e4r5t6y&U");
+    guestData.add("Aнастасія");
+    return guestData;
+    }
+
+
+
+}
+    /*
+    private String email;
+    private String password;
+    private String userName;
+
+
+
+    private RegistrationData(){
+        email = "nastacy@yahoo.com";
+        password = "1q2w3e4r5t6y&U";
+        userName = "Aнастасія";
+    }
+
+    // setters
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+     public void setUserName(String userName) {this.userName = userName;}
+
+    // getters
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUserName(){return userName;}
+
+    @Override
+    public String toString() {
+    return "User [email=" + email +"userName="+ userName+ ", password=" + password + "]";
+    }
+}
+
+     */
